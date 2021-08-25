117b7bca6e43220d4e6cfbb8367fcb65c25e9f84
Jim <the.jim.farrugia@gmail.com>
2021-08-25 13:01:13 +1000

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// * Instructions:
+// * Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+// * With the ROT13 cipher, the values of the letters are shifted by 13 places.
+// * Thus A ↔ N, B ↔ O and so on.
+// * All letters will be uppercase.
+// * Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+function rot13(str) {
+  return str;
+}
+
+console.log(rot13("SERR PBQR PNZC"));
