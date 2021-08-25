b0e36a32abfab8bfd8a92098b64e6f3dbda17ac7
Jim <the.jim.farrugia@gmail.com>
2021-08-25 13:42:03 +1000

    add helper function to convert a single letter using ROT13

--- script.js
+++ script.js
@@ -13,13 +13,32 @@
 //   then subtract 13 from the character code and convert to a letter.
 // - Might be helpful to have a helper to check for non-alphabetic characters.
 
+// * Character codes:
+// * A => 65; M => 77; N => 78; Z => 90; " " => 32;
+
 // TODO:
-// Write a helper function to convert a single letter using ROT13.
-// Iterate through the chars in the input string,
-// convert each alphabetic char and skip over any other char.
+// - Write a helper function to convert a single letter using ROT13.
+// - Iterate through the chars in the input string,
+//   convert each alphabetic char and skip over any other char.
+
+// * Helper function to convert a single letter using ROT13.
+function convertLetter(letter) {
+  const charCode = letter.charCodeAt(0);
+  if (charCode >= 65 && charCode <= 77) {
+    return String.fromCharCode(charCode + 13);
+  }
+  if (charCode >= 78 && charCode <= 90) {
+    return String.fromCharCode(charCode - 13);
+  }
+  // return the starting value if it's not a capital letter.
+  return letter;
+}
 
 function rot13(str) {
   return str;
 }
 
-console.log(rot13("SERR PBQR PNZC"));
+console.log(convertLetter("N"));
+console.log(convertLetter("B"));
+console.log(convertLetter("?"));
+// console.log(rot13("SERR PBQR PNZC"));
