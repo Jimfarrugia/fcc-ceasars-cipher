e85034718dde0e3d4b8c5f5dcedf758d03fb6535
Jim <the.jim.farrugia@gmail.com>
2021-08-25 13:22:31 +1000

    add comments with plan for solution

--- script.js
+++ script.js
@@ -5,6 +5,19 @@
 // * All letters will be uppercase.
 // * Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 
+// Thoughts:
+// - Use character codes.
+// - If a given letter is between A and M
+//   then add 13 to the character code and convert to a letter.
+// - If a given letter is between N and Z
+//   then subtract 13 from the character code and convert to a letter.
+// - Might be helpful to have a helper to check for non-alphabetic characters.
+
+// TODO:
+// Write a helper function to convert a single letter using ROT13.
+// Iterate through the chars in the input string,
+// convert each alphabetic char and skip over any other char.
+
 function rot13(str) {
   return str;
 }
